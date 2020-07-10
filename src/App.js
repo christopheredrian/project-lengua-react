6c68f5f308334780f43de26b5cd37e62157d07aa
Christopher Espiritu <christopheredrian@gmail.com>
2020-07-09 23:21:34 -0400

    project lengua api playground

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Container, Row, Button, Col} from 'react-bootstrap';
+import 'tachyons';
+import axios from 'axios'
+
+const ENDPOINT = 'https://project-lengua.herokuapp.com/api/sentiment/sentence';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [sentence, setSentence] = useState("");
+    const [response, setResponse] = useState("");
+
+    const checkSentimentHandler = () => {
+
+        axios({
+            method: 'get',
+            url: `${ENDPOINT}?sentence=${sentence}`,
+        }).then(({data}) => {
+            setResponse(data);
+        });
+
+    };
+
+    return (
+        <div className='vh-100 pt-3'>
+            <Container>
+
+                <Row>
+                    <Col className='text-center'>
+                        <h1 className='pb-3 border-bottom mb-5'>
+                            Project Lengua
+                        </h1>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={6}>
+
+                        <Row>
+                            <Col>
+                                <Form.Control
+                                    as="textarea"
+                                    // rows="10"
+                                    style={{height: "60vh"}}
+                                    onChange={e => setSentence(e.target.value)}
+                                />
+                            </Col>
+                        </Row>
+
+                        <Row className='mt-3'>
+                            <Col className='text-right'>
+                                <Button
+                                    size={'lg'}
+                                    onClick={checkSentimentHandler}
+                                >
+                                    Check Sentiment
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={6}>
+                        {
+                            response &&
+                            <pre><code>{JSON.stringify(response, null, 2)}</code></pre>
+                        }
+                    </Col>
+                </Row>
+
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
