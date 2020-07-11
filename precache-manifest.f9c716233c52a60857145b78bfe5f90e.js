856b9cd0f473ca9326af939972ca01b80de4a311
Christopher Espiritu <christopheredrian@gmail.com>
2020-07-10 21:36:57 -0400

    Updates

---
+++ precache-manifest.f9c716233c52a60857145b78bfe5f90e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c67bf77b120d867e422bd33af85beee",
+    "url": "/project-lengua-react/index.html"
+  },
+  {
+    "revision": "2562228648cfa941e415",
+    "url": "/project-lengua-react/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "414078095602e0b56773",
+    "url": "/project-lengua-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2562228648cfa941e415",
+    "url": "/project-lengua-react/static/js/2.60b9f438.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/project-lengua-react/static/js/2.60b9f438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414078095602e0b56773",
+    "url": "/project-lengua-react/static/js/main.8a2c7b83.chunk.js"
+  },
+  {
+    "revision": "0ac17bdbe4084ef1dd09",
+    "url": "/project-lengua-react/static/js/runtime-main.e2e1757e.js"
+  }
+]);
